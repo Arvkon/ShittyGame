ddedd043210654af688914f708062faf1790958c
Arvid Kongstad <arvid@getnarrative.com>
2016-05-05 19:00:55 +0200

    Change background color.

--- ShittyGame/ViewController.swift
+++ ShittyGame/ViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
+import SwiftColor
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor("91ccec")
     }
 }
