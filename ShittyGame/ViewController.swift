85bb487f865926f693fc10caac2dadff76814e6a
Arvid Kongstad <arvid@getnarrative.com>
2016-05-05 22:50:00 +0200

    Animate in cover view from below when tapping die image.

--- ShittyGame/ViewController.swift
+++ ShittyGame/ViewController.swift
@@ -10,14 +10,59 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor("91ccec")
         
         view.addSubview(dieSideImageView)
+        view.addSubview(coverView)
         
-        constrain(dieSideImageView) { dieSideImageView in
+        constrain(dieSideImageView, coverView) { dieSideImageView, coverView in
             dieSideImageView.centerX == dieSideImageView.superview!.centerX
             dieSideImageView.centerY == dieSideImageView.superview!.centerY
+            
+            coverView.left   == coverView.superview!.left
+            coverView.right  == coverView.superview!.right
+            coverView.height == coverView.superview!.height
+        }
+        
+        constrain(coverView, replace: coverViewTopConstraint) { coverView in
+            coverView.top == coverView.superview!.bottom
         }
     }
     
     // MARK: - Views
     
-    let dieSideImageView = UIImageView(image: UIImage(named: "DieSide"))
+    lazy var dieSideImageView: UIImageView = {
+        let dieSideImageView = UIImageView(image: UIImage(named: "DieSide"))
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: .tapDetected)
+        dieSideImageView.addGestureRecognizer(tapRecognizer)
+        dieSideImageView.userInteractionEnabled = true
+        
+        return dieSideImageView
+    }()
+    
+    lazy var coverView: UIView = {
+        let coverView = UIView(frame: .zero)
+        coverView.backgroundColor = UIColor.brownColor()
+        
+        return coverView
+    }()
+    
+    // MARK: - Properties
+    
+    let coverViewTopConstraint = ConstraintGroup()
+    
+    var coverViewBottomOffset: CGFloat = 0.0
+    
+    // MARK: - Methods
+    
+    func tapDetected() {
+        coverViewBottomOffset += view.bounds.height / 15
+        constrain(coverView, replace: coverViewTopConstraint) { coverView in
+            coverView.top == coverView.superview!.bottom - coverViewBottomOffset
+        }
+        UIView.animateWithDuration(0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
+
+private extension Selector {
+    static let tapDetected = #selector(ViewController.tapDetected)
 }
