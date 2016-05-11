6f39d451bedf964cdb78d8e5fb926681c6150390
Arvid Kongstad <arvid@getnarrative.com>
2016-05-11 21:28:27 +0200

    Make sure the same evil laugh is not played two times in a row.

--- ShittyGame/Sound.swift
+++ ShittyGame/Sound.swift
@@ -12,9 +12,15 @@ enum Sound: String {
     static let evilLaughs = [EvilLaugh1, EvilLaugh2, EvilLaugh3, EvilLaugh4, EvilLaugh5, EvilLaugh6]
     static let allValues  = evilLaughs + [KaChing]
     
+    static var indexOfPreviousEvilLaugh = -1
+    
     static func randomEvilLaugh() -> Sound {
         let count = UInt32(Sound.evilLaughs.count)
-        let index = Int(arc4random_uniform(count))
+        var index = Int(arc4random_uniform(count))
+        while index == indexOfPreviousEvilLaugh {
+            index = Int(arc4random_uniform(count))
+        }
+        indexOfPreviousEvilLaugh = index
         return Sound.evilLaughs[index]
     }
 }
